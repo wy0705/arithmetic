c9c8c904b15fcb50d1a0e6264651e8c708a5429e
wy0705 <2652918156@qq.com>
2021-06-07 18:37:52 +0800

    111

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import com.Solution;
+import com.Test1;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        //1
+        /*Test1 test1=new Test1();
+        int[] a=test1.twoSum(new int[]{2,7,11,15},9);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }*/
+
+        //2
+        Solution solution=new Solution();
+        //System.out.println(solution.romanToInt("LVIII"));
+
+        //7
+        System.out.println(solution.reverse(-123));
+    }
+}
